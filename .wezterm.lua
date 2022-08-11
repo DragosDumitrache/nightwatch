959781ed4b199e114583a7014d67e83a3fde2036
Dragos Dumitrache <dragos@afterburner.dev>
2022-08-11 08:18:09 +0100

    Work on wezterm config for pane splitting

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,55 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
+local act = wezterm.action
 
+wezterm.on('update-right-status', function(window, pane)
+  window:set_right_status(window:active_workspace())
+end)
 wezterm.on("gui-startup", function()
   local tab, pane, window = mux.spawn_window{}
   window:gui_window():maximize()
 end)
 
 return {
+  keys = {
+    {
+      key = 'w',
+      mods = 'CMD',
+      action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+        -- Switch to the default workspace
+    {
+      key = 'y',
+      mods = 'CTRL|SHIFT',
+      action = act.SwitchToWorkspace {
+        name = 'default',
+      },
+    },
+    -- Switch to a monitoring workspace, which will have `top` launched into it
+    {
+      key = 'u',
+      mods = 'CTRL|SHIFT',
+      action = act.SwitchToWorkspace {
+        name = 'monitoring',
+        spawn = {
+          args = { 'top' },
+        },
+      },
+    },
+    -- Create a new workspace with a random name and switch to it
+    { key = 'i', mods = 'CTRL|SHIFT', action = act.SwitchToWorkspace },
+    -- Show the launcher in fuzzy selection mode and have it list all workspaces
+    -- and allow activating one.
+    {
+      key = '9',
+      mods = 'ALT',
+      action = act.ShowLauncherArgs {
+        flags = 'FUZZY|WORKSPACES',
+      },
+    },
+  },
+
+  
   font = wezterm.font 'Fira Code',
   color_scheme = 'Snazzy' ,
   font_size = 12,
